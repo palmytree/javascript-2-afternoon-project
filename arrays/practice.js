9b1cf31736d499976daf1258f4dc25c8c9554b95
palmytree <jeff@palmytree.com>
2020-04-30 15:05:35 -0500

    finished afternoon project

--- arrays/practice.js
+++ arrays/practice.js
@@ -1,4 +1,4 @@
-/*
+/* 
   Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
@@ -8,122 +8,380 @@
 
 ////////// PROBLEM 1 //////////
 
+// Do not edit the code below.
+var arr = [10, 20, 30];
+// Do not edit the code above.
+
 /*
-  Create an object called me.
-  Give it a key of name with the value being your name, and another key of age with the value being your age.
-  Then alert your name using dot notation.
+  Create a function named 'first' that is given 'arr' as an argument.
+  Return the first item in the given array.
 */
 
-//Code here
+const first = (arr) => arr[0];
 
 ////////// PROBLEM 2 //////////
 
-/*
-  Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
-  Have the values to those keys be strings that are equal to your favorite thing in that category.
-*/
-
-//Code here
+// Do not edit the code below.
+var arr = [40, 50, 60];
+// Do not edit the code above.
 
 /*
-  After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
-  and then another key named 'brand' with the value being your favorite brand.
+  Create a function named 'last' that is given 'arr' as an argument. 
+  Return the last item in the given array.
 */
 
-//Code here
-
-/*
-  Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
-  and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-*/
-
-//Code here
+const last = (arr) => arr[arr.length - 1];
 
 ////////// PROBLEM 3 //////////
 
-/*
-  Create an empty Object called backPack.
-  Now, create a variable called item and set it equal to the string 'firstPocket'.
-  Using bracket notation and the item variable, add a 'firstPocket' key (or property) to backPack.
-  Set the value of that key to 'chapstick'.
-  Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
-*/
-
-//Code here
+// Do not edit the code below.
+var family = ["Tyler", "Jordyn", "Ryan", "Chelsey", "Ireland"];
+// Do not edit the code above.
 
 /*
-  After you do the above, alert your entire backPack object.
+  Create a function named 'looper' that is given family as it's only argument. 
+  Loop through the given array and alert every item in the array.
 */
 
-//Code here
-
-/*
-You probably noticed that it just alerted [object Object].
-Alerting to see the data in your Object doesn't work so well.
-Instead, console.log your whole backPack object and then check out the console.
-*/
-
-//Code here
+const looper = (arr) => {
+  for (i = 0; i < arr.length; i++) {
+    alert(arr[i]);
+  }
+};
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
-var user2 = {
-  name: 'Bryan',
-  age: 24,
-  pwHash: 'U+Ldlngx2BYQk',
-  email: 'BryanSmith33@gmail.com',
-  birthday: '05/02/1990',
-  username: 'bryansmith33'
-};
+var letters = ["A", "B", "C", "D", "E"];
 // Do not edit the code above.
 
 /*
-  Let's say I, the user, decided to change my name and email address to the following:
-  name -> 'Bryan G. Smith' and email -> 'bryan.smith@devmounta.in'.
-  Make that change without modifying the original object code above.
+  Write a function called reversedLooper that is given letters as it's only argument. 
+  Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+const reversedLooper = (arr) => {
+  for (i = arr.length - 1; i >= 0; i--) {
+    alert(arr[i]);
+  }
+};
+
+////////// PROBLEM 5 //////////
+
+// Do not edit the code below.
+var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
+// Do not edit the code above.
+
+/*
+  Write a function named evenFinder that is given nums as it's only argument.
+  Return an array that contains the even numbers from the nums array.
+*/
+
+const evenFinder = (nums) => {
+  let arr = [];
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) arr.push(nums[i]);
+  }
+  return arr;
+};
+
+console.log(evenFinder(nums));
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
-////////// PROBLEM 5 //////////
-
-/*
-  Create an empty object called methodCollection.
-*/
-
-//Code Here
-
-/*
-  Now add two methods (functions that are properties on objects) to your methodCollection object.
-  One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
-*/
-
-//Code Here
-
-/*
-  Now call your alertHello and logHello methods.
-*/
-
-//Code Here
-
 ////////// PROBLEM 6 //////////
 
+// Do not edit the code below.
+var numbersArray = [1, 2, 34, 54, 55, 34, 32, 11, 19, 17, 54, 66, 13];
+// Do not edit the code above.
+
 /*
-  Create a function called makePerson which takes in name, birthday, ssn as its parameters.
-  Return a new object with all of the information that you passed in.
+  Write a function called divider that is given one argument, numbersArray.
+  Have divider return an array with the first item in the array being the evens array (all the even values from numbersArray)
+  and the second item in the array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+const divider = (arr) => {
+  let evenArr = [];
+  let oddArr = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenArr.push(arr[i]);
+    } else {
+      oddArr.push(arr[i]);
+    }
+  }
+  let returnArr = [evenArr, oddArr];
+  return returnArr;
+};
 
 ////////// PROBLEM 7 //////////
 
+// Do not edit the code below.
+var getRandomArbitrary = function () {
+  return Math.floor(Math.random() * 30);
+};
+// Do not edit the code above.
+
+/* 
+  var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+  Above you're given a function (getRandomArbitrary) that will return a random number between 0 and 30.
+  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+  Write a function named finder that will take in an array as an argument.
+  In the function create a variable called randomNumber and set it to the invocation of getRandomArbitrary.
+  Loop through the array to see if randomNumber is in the array. 
+  If it is, return true, if it's not, return false
+*/
+
+const finder = (arr) => {
+  let randomNumber = getRandomArbitrary();
+  let tf = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === randomNumber) {
+      tf = true;
+      break;
+    }
+  }
+  return tf;
+};
+
+////////// PROBLEM 8 //////////
+
+// Do not edit the code below.
+var myGroceryList = ["chips", "pizza", "hotpockets", "MtnDew", "corndogs"];
+// Do not edit the code above.
+
 /*
-  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
-  Return that object so that whenever you invoke makeCard, you get a brand new credit card.
+  Here we're going to write a function that mimics going shopping and checking things off of our grocery list and adding new items to our list.
+
+  Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
+  If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
+
+  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
+  In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
+
+  In both removeItem and addItem check to see if the 'myGroceryList' and 'item' arguments are truthy.
+  If they are not, return an empty array.
+
+  Here are some examples of calling your functions and what should be returned:
+  removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+  addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
+  removeItem(myGroceryList) --> [];
+  addItem() --> [];
+*/
+
+const removeItem = (arr, itemToRemove) => {
+  if (!arr || !itemToRemove) return [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === itemToRemove) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
+
+const addItem = (arr, itemToAdd) => {
+  if (!arr || !itemToAdd) return [];
+  arr.push(itemToAdd);
+  return arr;
+};
+
+////////// PROBLEM 9 //////////
+
+/*
+  Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
+*/
+
+const maker = () => {
+  let arr = [];
+  for (let i = 1; i < 216; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+////////// PROBLEM 10 //////////
+
+// Do not edit the code below.
+var numbers = [5, "9", 16, 19, "25", "34", 48];
+// Do not edit the code above.
+
+/*
+  Write a function called addTen that is given 'numbers' as it's only argument.
+  Return a new array after adding ten to each item in numbers. 
+  Your output should look like this -> [15, 19, 26, 29, 35, 44, 58]
+*/
+
+const addTen = (arr) => {
+  let rArr = []
+  for (let i = 0; i < arr.length; i++) {
+    rArr.push(Number(arr[i]) + 10);
+  }
+  return rArr;
+};
+
+////////// PROBLEM 11 //////////
+
+// Do not edit the code below.
+var num1 = Math.floor(Math.random() * 30);
+var num2 = Math.floor(Math.random() * 30);
+var arr1 = [];
+var arr2 = [];
+for (var i = 0; i < num1; i++) {
+  arr1.push(i);
+}
+for (var i = 0; i < num2; i++) {
+  arr2.push(i);
+}
+// Do not edit the code above.
+
+/*
+  Above is some code that adds a random number of values to both arr1 and arr2.
+  Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. 
+  Return the longer of the two arrays.
+*/
+
+const longer = (array1, array2) => {
+  if (array1.length > array2.length) {
+    return array1;
+  }
+  return array2;
+};
+
+/*
+  As a continuation of the previous problem, write another function called 'both'.
+  Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example).
+  'both' should return a new array with the matching numbers found in both arr1 and arr2.
+
+  Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
+*/
+
+const both = (array1, array2) => {
+  let returnArr = [];
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) returnArr.push(array1[i]);
+    }
+  }
+  return returnArr;
+};
+
+////////// PROBLEM 12 //////////
+
+// Do not edit the code below.
+var devMountainEmployees = [];
+
+var joe = {
+  name: "Joe",
+  position: "Instructor",
+  spiritAnimal: "Honey Badger",
+};
+
+var cahlan = {
+  name: "Cahlan",
+  position: "CEO",
+  spiritAnimal: "butterfly",
+};
+
+var ryan = {
+  name: "Ryan",
+  position: "Marketing",
+  spiritAnimal: "fox",
+};
+
+var colt = {
+  name: "Colt",
+  position: "Everything really",
+  spiritAnimal: "Young Male Horse",
+};
+// Do not edit the code above.
+
+/*
+  Above you're given an empty array and four variables containing objects. 
+  Fill the devMountainEmployees array with those four objects. Do this in either
+  the global scope or in a function. If done in a function, invoke it.
+  After that, console.log the length of the Array and make sure that it's equal to 4. 
+*/
+
+devMountainEmployees.push(joe, cahlan, ryan, colt);
+console.log(devMountainEmployees.length);
+
+/*
+  Now let's say Cahlan has to take a leave of absence.
+  Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
 */
 
 //Code Here
+
+devMountainEmployees.splice(devMountainEmployees.indexOf(cahlan), 1);
+
+/*
+  A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects) of data is to have an array full of objects. 
+  Create an empty array called users.
+*/
+
+const users = [];
+
+/*
+  Now add three user objects to your users array. Each user object should contain the following properties: name, email, password, username.
+
+  Include the following user1 object as one of the objects in your array.
+  
+  MAKE SURE TO DO THIS IN THE SECTION BELOW WHERE USER1 IS DECLARED
+*/
+
+// Do not edit the code below.
+var user1 = {
+  name: "Mark McIver",
+  email: "mark.mciver@devmounta.in",
+  password: "hunter2",
+  username: "ihazcode",
+};
+// Do not edit the code above.
+
+const createUser = (name, email, password, username) => {
+  return {
+    name,
+    email,
+    password,
+    username,
+  };
+};
+
+for (let i = 1; i < 3; i++) {
+  users.push(
+    createUser(
+      `User${i}`,
+      `email${i}@email.com`,
+      `password${i}`,
+      `username${i}`
+    )
+  );
+}
+
+users.push(user1);
+
+/*
+  Now you have a very common data structure. 
+  Twitter is a good use case.
+  It's easy to imagine that your followers list on Twitter is an array full of objects and those objects contain properties about the specific person you follow.
+
+  Now let's say that Mark decided to delete his account.
+  Loop through your array of objects until you find Mark's account (use his email, mark.mciver@devmounta.in, to find him).
+  Once you find the array index he's located in, delete him from the array.
+*/
+
+const deleteUser = (email) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      users.splice(i, 1);
+    }
+  }
+};
+
+deleteUser(user1.email);
+
+/*
+  The activity we just did is very much how data works in 'the real world'.
+*/
--- callbacks/practice.js
+++ callbacks/practice.js
@@ -27,7 +27,7 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+const first = (arr, cbfn) => cbfn(arr[0])
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +47,7 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+const last = (arr, cbfn) => cbfn(arr[arr.length - 1])
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +65,8 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+const multiply = (x, y, cbfn) => cbfn(x * y)
+
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +85,9 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+const contains = (arr, name, cbfn) => {
+  arr.indexOf(name) >= 0 ? cbfn(true) : cbfn(false)
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +108,13 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+const uniq = (arr, cbfn) => {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i]) === i) newArr.push(arr[i])
+  }
+  return cbfn(newArr)
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -122,7 +131,11 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+const each = (arr, cbfn) => {
+  for (let i = 0; i < arr.length; i++) {
+    cbfn(arr[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -139,7 +152,11 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+const getUserById = (arr, id, cbfn) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return cbfn(arr[i])
+  }
+}
 
 // Do not edit the code below.
 var users = [
